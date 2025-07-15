191ee2f679630bb5bffb9c8410ee71b8eaa12e61
Wellingtoncvs <wellington.santos@addasoftwares.com>
2025-07-15 18:43:42 -0300

    Implemented Tests for insert UserService

--- src/main/java/com/devsuperior/dscommerce/services/AuthService.java
+++ src/main/java/com/devsuperior/dscommerce/services/AuthService.java
@@ -12,7 +12,7 @@ public class AuthService {
 	@Autowired
 	private UserService userService;
 	
-	public void validateSelfOrAdmin(long userId) {
+	public void validateSelfOrAdmin(Long userId) {
 		User me = userService.authenticated();
 //		if (!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)) {
 //      throw new ForbiddenException("Access denied");
