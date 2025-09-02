13231aa2733d6450d0c392a3d326cb50969a5906
Wellingtoncvs <wellington.santos@addasoftwares.com>
2025-09-02 19:00:48 -0300

    insertShouldReturnProductDTOCreatedWhenAdminLogged git commit -m insertShouldReturnProductDTOCreatedWhenAdminLogged

--- src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -13,7 +13,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.tests.TokenUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,15 +29,39 @@ public class ProductControllerIT {
 
 	@Autowired
 	private MockMvc mockMvc;
-
+	
+	@Autowired
+	private TokenUtil tokenUtil;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private String clientUsername, clientPassword, adminUsername, adminPassword;
+	private String clientToken, adminToken, invalidToken;
 	private String productName;
-	private String adminToken;
+	
+	private Product product;
+	private ProductDTO productDTO;
 
 	@BeforeEach
-	void setUp() {
+	void setUp() throws Exception {
+		
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
+		adminUsername  = "alex@gmail.com";
+		adminPassword  = "123456";
 
 		productName = "Macbook";
-
+		
+		clientToken  = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
+		adminToken   = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+		invalidToken = adminToken + "xpto"; // Simulates wrong password
+		
+		Category category = new Category(2L, "Eletro");
+		product = new Product(null, "Console PlayStation 5", "Lorem ipsum, door set amet consectetur adipisicing elit.",3999.90, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
+		product.getCategories().add(category);
+		productDTO = new ProductDTO(product);
+		
 	}
 
 	@Test
@@ -42,8 +73,7 @@ public class ProductControllerIT {
 		result.andExpect(jsonPath("$.content[0].id").value(1L));
 		result.andExpect(jsonPath("$.content[0].name").value("The Lord of the Rings"));
 		result.andExpect(jsonPath("$.content[0].price").value(90.5));
-		result.andExpect(jsonPath("$.content[0].imgUrl").value(
-				"https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+		result.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
 
 	}
 
@@ -51,27 +81,39 @@ public class ProductControllerIT {
 	public void findAllShouldReturnPageWhenNameParamIsNotEmpty() throws Exception {
 
 		ResultActions result = mockMvc
-				.perform(get("/products?name={productName}", productName).accept(MediaType.APPLICATION_JSON));
+				.perform(get("/products?name={productName}", productName)
+				.accept(MediaType.APPLICATION_JSON));
 
 		result.andExpect(status().isOk());
 		result.andExpect(jsonPath("$.content[0].id").value(3L));
 		result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
 		result.andExpect(jsonPath("$.content[0].price").value(1250.0));
-		result.andExpect(jsonPath("$.content[0].imgUrl").value(
-				"https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+		result.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
 
 	}
 
+	
 	@Test
 	public void insertShouldReturnProductDTOCreatedWhenAdminLogged() throws Exception {
 
-		String jsonBody = "";
+		String jsonBody= objectMapper.writeValueAsString(productDTO);
 
 		ResultActions result = mockMvc
 				.perform(post("/products")
-				.header("Autorization,", "Bearer" + adminToken)
-				.content(jsonBody).accept(MediaType.APPLICATION_JSON));
-
+				.header("Authorization,", "Bearer" + adminToken)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andDo(MockMvcResultHandlers.print());
+		
+		result.andExpect(status().isCreated());
+		result.andExpect(jsonPath("$.id").value(26L));
+		result.andExpect(jsonPath("$.name").value("Console PlayStation 5"));
+		result.andExpect(jsonPath("$.description").value("Lorem ipsum, door set amet consectetur adipisicing elit."));
+		result.andExpect(jsonPath("$.price").value(3999.90));
+		result.andExpect(jsonPath("$.imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+		result.andExpect(jsonPath("$.categories[0].id").value(2L));
+		
 	}
 
 }
